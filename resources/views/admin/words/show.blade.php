603eb4206128b94386fbfab09e89a8c8b5a6c39d
EricaMarzi <erica.marzi.1993@gmail.com>
2024-03-21 22:38:25 +0100

    show + style logo navbar

--- resources/views/admin/words/show.blade.php
+++ resources/views/admin/words/show.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.app')
+@section('title', 'Word')
 
 @section('content')
-<p>show</p>
+<header class="mt-4">
+    <h1 class="text-capitalize">{{$word->term}}</h1>
+</header>
+
+
+
+<p>{{$word->description}}</p>
+<div>
+    <strong>Data di creazione:</strong> {{$word->getFormattedDate('created_at')}}
+    <strong class="ms-5">Ultima modifica:</strong> {{$word->getFormattedDate('updated_at')}}
+</div>
+
+<hr>
+<footer class="d-flex justify-content-between align-items-center mt-5">
+    <a href="{{route('admin.words.index')}}" class="btn btn-secondary">
+        <i class="fas fa-arrow-left"></i>
+    </a>
+    <div class="d-flex gap-2">
+        <a href="{{route('admin.words.edit', $word)}}" class="btn btn-sm btn-warning">
+            <i class="fas fa-pencil me-2"></i>Modifica
+        </a>
+
+        <form action="{{route('admin.words.destroy', $word)}}" method="POST" class="delete-form">
+            @csrf
+            @method('DELETE')
+            <button type='submit' class="btn btn-sm btn-danger">
+                <i class="fas fa-trash me-2"></i>Elimina
+            </button>
+        </form>
+    </div>
+</footer>
 @endsection
