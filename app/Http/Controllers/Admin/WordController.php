1c0ebf71c283b4fe7aa7bcdb79807ce19af2df0e
FrancescoM95 <mon95fran@gmail.com>
2024-03-21 16:30:18 +0100

    controllers update

--- app/Http/Controllers/Admin/WordController.php
+++ app/Http/Controllers/Admin/WordController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\Word;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class WordController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\HomeController as AdminHomeController;
+use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\WordController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,16 +19,25 @@ use Illuminate\Support\Facades\Route;
 
 //# ROTTE GUEST
 
-Route::get('/', function () {
-    return view('guest.home');
-});
+Route::get('/', GuestHomeController::class)->name('guest.home');
 
 
 //# ROTTE ADMIN
 
-Route::get('/admin', function () {
-    return view('admin.home');
-})->middleware(['auth'])->name('admin.home');
+Route::prefix('/admin')->middleware('auth')->name('admin.')->group(function () {
+
+    Route::get('', AdminHomeController::class)->name('home');
+    // Route::get('/words', [WordController::class, 'index'])->name('index');
+    // Route::get('/words/create', [WordController::class, 'create'])->name('create');
+    // Route::get('/words/{word}', [WordController::class, 'show'])->name('show');
+    // Route::get('/words/{word}/edit', [WordController::class, 'edit'])->name('edit');
+    // Route::post('/words', [WordController::class, 'store'])->name('store');
+    // Route::put('/words/{word}', [WordController::class, 'update'])->name('update');
+    // Route::delete('/words/{word}', [WordController::class, 'destroy'])->name('destroy');
+
+    Route::resource('words', WordController::class);
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
