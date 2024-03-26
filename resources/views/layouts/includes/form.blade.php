3c53e1f9bd7bb12ee5f165d98e83f9b970251b54
giamaf <gianluca.maffucci@hotmail.com>
2024-03-26 22:00:58 +0100

    added validation + displaying errors

--- resources/views/layouts/includes/form.blade.php
+++ resources/views/layouts/includes/form.blade.php
@@ -8,12 +8,34 @@
 
 <div class="col-md-6">
     <label for="term" class="form-label">Termine</label>
-    <input type="text" name="term" class="form-control" id="term" value="{{ old('term', $word->term) }}"
-        placeholder="Inserisci un termine...">
+    <input type="text" name="term"
+        class="form-control  @error('term') is-invalid @elseif (old('term', '')) is-valid @enderror"
+        id="term" value="{{ old('term', $word->term) }}" placeholder="Inserisci un termine...">
+    @error('term')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @else
+        <div class="form-text">
+            <p>Inserisci il termine</p>
+        </div>
+    @enderror
 </div>
+
 <div class="col-12">
     <label for="description" class="form-label">Descrizione</label>
-    <textarea name="description" class="form-control" id="description" rows="10">{{ old('description', $word->description) }}</textarea>
+    <textarea name="description"
+        class="form-control @error('description') is-invalid @elseif (old('description', '')) is-valid @enderror"
+        id="description" rows="10">{{ old('description', $word->description) }}</textarea>
+    @error('description')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+    @else
+        <div class="form-text">
+            <p>Inserisci la descrizione</p>
+        </div>
+    @enderror
 </div>
 
 <div class="col-12 d-flex justify-content-end gap-3">
