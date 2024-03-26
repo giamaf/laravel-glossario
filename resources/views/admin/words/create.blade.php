a2428829d7da4f57fc0f5bd6318b8b6a5effb917
giamaf <gianluca.maffucci@hotmail.com>
2024-03-26 21:15:26 +0100

    added unique form to create and edit view

--- resources/views/admin/words/create.blade.php
+++ resources/views/admin/words/create.blade.php
@@ -3,30 +3,14 @@
 @section('title', 'Word')
 
 @section('content')
+
+    {{-- Intestazione pagina --}}
     <header>
         <h1>Inserisci un nuovo termine</h1>
     </header>
     <hr>
-    <form class="row row-gap-3 " action="{{ route('admin.words.store') }}" method="POST">
-        @csrf
 
-        <div class="col-md-6">
-            <label for="term" class="form-label">Termine</label>
-            <input type="text" name="term" class="form-control" id="term" value="{{ old('term', $word->term) }}"
-                placeholder="Inserisci un termine...">
-        </div>
-        <div class="col-12">
-            <label for="description" class="form-label">Descrizione</label>
-            <textarea name="description" class="form-control" id="description" rows="10">{{ old('description', $word->description) }}</textarea>
-        </div>
+    {{-- Form --}}
+    @include('layouts.includes.form')
 
-        <div class="col-12 d-flex justify-content-end gap-3">
-            <button class="btn btn-sm btn-warning" type="reset">
-                <i class="fa-solid fa-eraser"></i>
-                Reset</button>
-            <button type="submit" class="btn btn-sm btn-success ">
-                <i class="fa-solid fa-floppy-disk"></i>
-                Salva</button>
-        </div>
-    </form>
 @endsection
---
+++ resources/views/layouts/includes/form.blade.php
@@ -0,0 +1,27 @@
+@if ($word->exsist)
+    <form class="row row-gap-3 " action="{{ route('admin.words.update', $word->id) }}" method="POST">
+        @method('PUT')
+    @else
+        <form class="row row-gap-3 " action="{{ route('admin.words.store') }}" method="POST">
+@endif
+@csrf
+
+<div class="col-md-6">
+    <label for="term" class="form-label">Termine</label>
+    <input type="text" name="term" class="form-control" id="term" value="{{ old('term', $word->term) }}"
+        placeholder="Inserisci un termine...">
+</div>
+<div class="col-12">
+    <label for="description" class="form-label">Descrizione</label>
+    <textarea name="description" class="form-control" id="description" rows="10">{{ old('description', $word->description) }}</textarea>
+</div>
+
+<div class="col-12 d-flex justify-content-end gap-3">
+    <button class="btn btn-sm btn-warning" type="reset">
+        <i class="fa-solid fa-eraser"></i>
+        Reset</button>
+    <button type="submit" class="btn btn-sm btn-success ">
+        <i class="fa-solid fa-floppy-disk"></i>
+        Salva</button>
+</div>
+</form>
